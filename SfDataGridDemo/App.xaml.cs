81169a0eb33e448f8da58dea212ccfb5b540093b
vijayarasan <vijayarasan.sivanandham@syncfusiom.com>
2024-02-28 12:23:54 +0530

    ES-872377 Sample Added

---
+++ SfDataGridDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {           
+        }
+    }
+}
